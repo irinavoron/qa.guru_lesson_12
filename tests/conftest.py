8e81d62bd7786811e6df69445d189d126a59ca59
Irina.Voron <yarutich.irina@gmail.com>
2024-03-19 13:53:28 +0300

    Add attach.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,19 @@
 import pytest
 from selene import browser
 
+from utils import attach
+
 
 @pytest.fixture(autouse=True)
 def browser_settings():
     browser.config.base_url = 'https://github.com/'
     browser.config.window_width = 1896
-    browser.config.window_height =1080
+    browser.config.window_height = 1080
 
     yield
+
+    attach.add_sceenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
 
     browser.quit()
--- tests/test_github.py
+++ tests/test_github.py
@@ -12,15 +12,15 @@ def test_github_issues():
     with allure.step('Open main page'):
         browser.open('/')
 
-    with allure.step('Search for repository {repo}'):
+    with allure.step('Search for repository'):
         browser.element('.search-input').click()
         browser.element('#query-builder-test').type('irinavoron/qa.guru_lesson_12').press_enter()
 
-    with allure.step('Go to the repository {repo}'):
+    with allure.step('Go to the repository'):
         browser.element(by.link_text('irinavoron/qa.guru_lesson_12')).click()
 
     with allure.step('Open the Issues tab'):
         browser.element('#issues-tab').click()
 
-    with allure.step('The issue with the text in title should be displayed on the page {text}'):
+    with allure.step('The issue with the text in title should be displayed on the page'):
         browser.element('[aria-label=Issues][role=group]').should(have.text('Issue created for the homework'))
