2843cc767e54b4e2ed14b8420b1244d6ddcf7964
Irina.Voron <yarutich.irina@gmail.com>
2024-03-19 13:37:36 +0300

    Add allure

--- tests/test_github.py
+++ tests/test_github.py
@@ -1,10 +1,26 @@
+import allure
 from selene import browser, by, have
 
 
-def test_github_issue():
-    browser.open('/')
-    browser.element('.search-input').click()
-    browser.element('#query-builder-test').type('irinavoron/qa.guru_lesson_12').press_enter()
-    browser.element(by.link_text('irinavoron/qa.guru_lesson_12')).click()
-    browser.element('#issues-tab').click()
-    browser.element('[aria-label=Issues][role=group]').should(have.text('Issue created for the homework'))
+@allure.label('owner', 'irinavoron')
+@allure.feature('Issues tab')
+@allure.story('Issue with the indicated text in the title should be displayed on the page')
+@allure.title('Issue with the indicated text in the title should be displayed on the page')
+def test_github_issues():
+    allure.dynamic.link('https://github.com/')
+
+    with allure.step('Open main page'):
+        browser.open('/')
+
+    with allure.step('Search for repository {repo}'):
+        browser.element('.search-input').click()
+        browser.element('#query-builder-test').type('irinavoron/qa.guru_lesson_12').press_enter()
+
+    with allure.step('Go to the repository {repo}'):
+        browser.element(by.link_text('irinavoron/qa.guru_lesson_12')).click()
+
+    with allure.step('Open the Issues tab'):
+        browser.element('#issues-tab').click()
+
+    with allure.step('The issue with the text in title should be displayed on the page {text}'):
+        browser.element('[aria-label=Issues][role=group]').should(have.text('Issue created for the homework'))
