b999ba60762f4cba092593add5cb7b8cf3dafe1c
Irina.Voron <yarutich.irina@gmail.com>
2024-03-19 18:11:03 +0300

    Add selenoid

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,29 @@
 import pytest
 from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(autouse=True)
 def browser_settings():
+    options = Options()
+
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "122.0",
+        "selenoid:options": {
+            "enableVideo": True
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
+
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_height = 1080
     browser.config.window_width = 1896
